8e83c897eda5c1d230d435d61371de4b67f3d835
Marcin Kubacki <niemetal@gmail.com>
2014-09-04 15:49:45 +0200

    Ended hudd

--- heuristics.py
+++ heuristics.py
@@ -19,7 +19,18 @@ def heuristicHUDD(feasible_sequence,activities):
 				activities_counts_dict[activity_number] += 1
 			else:
 				activities_counts_dict[activity_number] = 1
-	print activities_counts_dict
+	activities_divided_dict = {}
+	for key in activities_counts_dict:
+		activities_divided_dict[key] = Rational(activities[key].overall_processing_demand, activities_counts_dict[key])
+	makespan=0
+	for feasible_sequence_part in feasible_sequence:
+		makespan_equation = 0
+		m = symbols("m",positive=True)
+		for activity_number in feasible_sequence_part:
+			makespan_equation += (activities_divided_dict[activity_number]/m)**activities[activity_number].processing_function_root
+		makespan += solve(makespan_equation -1, m)[0]
+	return makespan
 
-print heuristic1M([[0],[0,1],[2]],[Activity(0,4,2),Activity(1,2,2),Activity(2,2,1)])
+
+print heuristicHUDD([[0],[0,1],[2]],{0:Activity(0,20,2),1:Activity(1,30,2),2:Activity(2,40,1)})
 
